3d42d59c1b6426b3d0cb683a7bf41cdc98e16a33
henry <143973064+henryzdev@users.noreply.github.com>
2024-02-14 15:03:34 -0300

    Add files via upload

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,7 +1,7 @@
 package config
 
 import (
-
+	"errors"
 	"os"
 )
 
@@ -16,13 +16,21 @@ var (
 )
 
 func LoadConfig() error {
-	MPurl = "https://api.mercadopago.com/v1/payments"
-	
+	MPurl = os.Getenv("MP_URL")
+	if MPurl == "" {
+		return errors.New("MP_URL not found")
+	}
 	RabbitmqURL = os.Getenv("RABBITMQ_URL")
-	
-	MpToken = "APP_USR-2263346854076132-011400-800633c8a8e6697eff787a71b9a4dade-1628717800"
-
-	MpUrlCCToken = "https://api.mercadopago.com/v1/card_tokens"
-	
+	if RabbitmqURL == "" {
+		return errors.New("RABBITMQ_URL not found")
+	}
+	MpToken = os.Getenv("MP_TOKEN")
+	if MpToken == "" {
+		return errors.New("MP_TOKEN not found")
+	}
+	MpUrlCCToken = os.Getenv("MP_URL_CREDIT_CARD")
+	if MpUrlCCToken == "" {
+		return errors.New("MP_URL_CREDIT_CARD not found")
+	}	
 	return nil
 }
